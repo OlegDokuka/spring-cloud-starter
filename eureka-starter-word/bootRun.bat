4d5aa8c11ec72f2640348983f29045fe3912f3f8
shadowgun@i.ua <shaman21027>
2016-07-07 18:40:53 +0300

    optimize app config, enable both config first and discovery fist configurations

--- eureka-starter-word/bootRun.bat
+++ eureka-starter-word/bootRun.bat
@@ -1,10 +1,6 @@
-start %cd%/gradlew bootRun -Dspring.profiles.active=subject
-ping 127.0.0.1 -n 6 > nul
-start %cd%/gradlew bootRun -Dspring.profiles.active=verb
-ping 127.0.0.1 -n 6 > nul
-start %cd%/gradlew bootRun -Dspring.profiles.active=article
-ping 127.0.0.1 -n 6 > nul
-start %cd%/gradlew bootRun -Dspring.profiles.active=adjective
-ping 127.0.0.1 -n 6 > nul
-start %cd%/gradlew bootRun -Dspring.profiles.active=noun
+start %cd%/gradlew bootRun -Dspring.profiles.active=subject,discovery-first-bootstrap
+start %cd%/gradlew bootRun -Dspring.profiles.active=verb,discovery-first-bootstrap
+start %cd%/gradlew bootRun -Dspring.profiles.active=article,discovery-first-bootstrap
+start %cd%/gradlew bootRun -Dspring.profiles.active=adjective,discovery-first-bootstrap
+start %cd%/gradlew bootRun -Dspring.profiles.active=noun,discovery-first-bootstrap
 
