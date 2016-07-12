7637d4cdb005ac2c62f3495a3106df86c54b8d44
shadowgun@i.ua <shaman21027>
2016-07-12 16:38:35 +0300

    lab 8 preparation and config changes

--- cloud-starter-client/src/main/java/com/example/cloud/starter/web/rest/LuckyWordResource.java
+++ cloud-starter-client/src/main/java/com/example/cloud/starter/web/rest/LuckyWordResource.java
@@ -1,12 +1,12 @@
 package com.example.cloud.starter.web.rest;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@ConfigurationProperties(prefix = "wordConfig")
+@RefreshScope
 public class LuckyWordResource {
 
     @Value("${lucky-word}") String luckyWord;
