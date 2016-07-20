4f5fe1d7f8f9834fd1c757cf39e9fc94e1cf8287
shadowgun@i.ua <shaman21027>
2016-07-20 18:24:28 +0300

    fix app path

--- handmade/eureka-starter-word/src/main/java/com/example/eureka/starter/word/web/rest/WordResource.java
+++ handmade/eureka-starter-word/src/main/java/com/example/eureka/starter/word/web/rest/WordResource.java
@@ -3,15 +3,17 @@ package com.example.eureka.starter.word.web.rest;
 import com.example.eureka.starter.word.domain.Word;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("/api/words")
 public class WordResource {
     @Value("${words}")
     private String words;
 
-    @RequestMapping("/")
+    @RequestMapping(method = RequestMethod.GET)
     public Word getWord() {
         String[] wordArray = words.split(",");
         int i = (int) Math.round(Math.random() * (wordArray.length - 1));
