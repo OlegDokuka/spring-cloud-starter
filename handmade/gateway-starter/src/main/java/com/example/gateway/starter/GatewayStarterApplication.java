22018c983201700adfe1a58b0fb17b3d384d399c
shadowgun@i.ua <shaman21027>
2016-07-20 18:24:10 +0300

    add reducing sentence using rxjs and angular js, fix issue with auth on gateway

--- handmade/gateway-starter/src/main/java/com/example/gateway/starter/GatewayStarterApplication.java
+++ handmade/gateway-starter/src/main/java/com/example/gateway/starter/GatewayStarterApplication.java
@@ -2,9 +2,8 @@ package com.example.gateway.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
@@ -13,8 +12,8 @@ import javax.servlet.Filter;
 
 @SpringBootApplication
 @EnableZuulProxy
-@RibbonClients
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class GatewayStarterApplication {
 	@Bean
 	public Filter shallowETagHeaderFilter() {
--- handmade/gateway-starter/src/main/resources/static/js/app.js
+++ handmade/gateway-starter/src/main/resources/static/js/app.js
@@ -1,52 +1,72 @@
-angular.module('sso', ['ngRoute', 'ngResource']).config(
-    function ($routeProvider) {
+(function () {
+    angular.module('sso', ['ngRoute', 'ngResource']).config(
+        function ($routeProvider) {
 
-        $routeProvider.otherwise('/');
-        $routeProvider.when('/', {
-            templateUrl: 'home.html',
-            controller: 'home'
-        }).when('/sentence', {
-            templateUrl: 'sentence.html',
-            controller: 'sentence'
-        });
+            $routeProvider.otherwise('/');
+            $routeProvider.when('/', {
+                templateUrl: 'home.html',
+                controller: 'home'
+            }).when('/sentence', {
+                templateUrl: 'sentence.html',
+                controller: 'sentence'
+            });
 
-    }).controller('navigation', function ($scope, $http, $window, $route) {
-    $scope.tab = function (route) {
-        return $route.current && route === $route.current.controller;
-    };
-    if (!$scope.user) {
-        $http.get('/uaa/user').success(function (data) {
-            try {
-                data = JSON.parse(data);
-            } catch (e) {
+        }).controller('navigation', function ($scope, $http, $window, $route) {
+        $scope.tab = function (route) {
+            return $route.current && route === $route.current.controller;
+        };
+        if (!$scope.user) {
+            $http.get('/uaa/user').success(function (data) {
+                try {
+                    data = JSON.parse(data);
+                } catch (e) {
 
-            }
-            $scope.user = data;
-            $scope.authenticated = true;
-        }).error(function () {
-            $scope.authenticated = false;
-        });
-    }
-    $scope.logout = function () {
-        $http.post('/logout', {}).then(function () {
-            delete $scope.user;
-            $scope.authenticated = false;
-            // Force reload of home page to reset all state after logout
-            $window.location.hash = '/';
-        }, function () {
-            delete $scope.user;
-            $scope.authenticated = false;
-            // Force reload of home page to reset all state after logout
-            $window.location.hash = '/';
-        });
-    };
-}).controller('home', function () {
-}).controller('sentence', function ($scope, $resource) {
-
-    $resource('/dashboard/message', {}).get({}, function (data) {
-        $scope.message = data.message;
-    }, function () {
-        $scope.message = '';
-    });
-
-});
+                }
+                $scope.user = data;
+                $scope.authenticated = true;
+            }).error(function () {
+                $scope.authenticated = false;
+            });
+        }
+        $scope.logout = function () {
+            $http.post('/logout', {}).then(function () {
+                delete $scope.user;
+                $scope.authenticated = false;
+                // Force reload of home page to reset all state after logout
+                $window.location.hash = '/';
+            }, function () {
+                delete $scope.user;
+                $scope.authenticated = false;
+                // Force reload of home page to reset all state after logout
+                $window.location.hash = '/';
+            });
+        };
+    }).controller('home', function () {
+    }).controller('sentence', ['$scope', 'Word', function ($scope, Word) {
+        setInterval(function () {
+            Rx.Observable
+                .concat(Rx.Observable.startAsync(function () {
+                    return Word.get({name: "subject"}).$promise;
+                }), Rx.Observable.startAsync(function () {
+                    return Word.get({name: "verb"}).$promise;
+                }), Rx.Observable.startAsync(function () {
+                    return Word.get({name: "article"}).$promise;
+                }), Rx.Observable.startAsync(function () {
+                    return Word.get({name: "adjective"}).$promise;
+                }), Rx.Observable.startAsync(function () {
+                    return Word.get({name: "noun"}).$promise;
+                }))
+                .map(function (it) {
+                    return it.word;
+                })
+                .reduce(function (a, b) {
+                    return a + b + " ";
+                }, "")
+                .subscribe(function (sentence) {
+                    $scope.sentence = sentence;
+                }, function () {
+                    $scope.sentence = "shit happens";
+                });
+        }, 5000);
+    }]);
+})();
